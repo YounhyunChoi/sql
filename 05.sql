668852270d90f4835cd92a1c4a22b6d018dfca50
yunghyunchoi <asl321@naver.com>
2023-01-19 17:41:12 +0900

    'a'

---
+++ 05.sql
@@ -0,0 +1,104 @@
+select avg(salary), max(salary), min(salary), sum(salary)
+from employees;
+
+select min(hire_date), max(hire_date)
+from employees;
+
+-- 과제: 최고월급과 최소월급의 차액을 조회하라.
+select max(salary) - min(salary)
+from employees;
+
+select count(*)
+from employees;
+
+-- 과제: 70번 부서원이 몇명인지 조회하라.
+select count(*)
+from employees
+where department_id = 70;
+
+select count(employee_id)
+from employees;
+
+select count(manager_id)
+from employees;
+
+select avg(commission_pct)
+from employees;
+
+-- 과제: 조직의 평균 커미션율을 조회하라.
+select avg(nvl(commission_pct, 0))
+from employees;
+
+select avg(all salary)
+from employees;
+
+select avg(distinct salary)
+from employees;
+
+-- 과제: 직원이 배치된 부서 개수를 조회하라.
+select count(distinct department_id)
+from employees;
+
+-- 과제: 매니저 수를 조회하라.
+select count(distinct manager_id)
+from employees;
+
+select department_id, count(employee_id)
+from employees
+group by department_id
+order by department_id;
+
+select employee_id
+from employees
+where department_id = 30;
+
+select department_id, job_id, count(employee_id)
+from employees
+group by department_id, job_id
+order by department_id;
+
+-- 과제: 직업별 사원수를 조회하라.
+select job_id, count(employee_id)
+from employees
+group by job_id;
+
+select department_id, max(salary)
+from employees
+group by department_id
+having department_id > 50;
+
+select department_id, max(salary) max_salary
+from employees
+group by department_id
+having max(salary) > 10000;
+
+select department_id, max(salary) max_sal
+from employees
+group by department_id
+having max_sal > 10000;
+
+select department_id, max(salary)
+from employees
+where department_id > 50
+group by department_id;
+
+select department_id, max(salary)
+from employees
+where max(salary) > 10000
+group by department_id;
+
+select job_id, sum(salary) payroll
+from employees
+where job_id not like '%REP%'
+group by job_id
+having sum(salary) > 13000
+order by payroll;
+
+-- 과제: 매니저ID, 매니저별 관리 직원들 중 최소 월급을 조회하라.
+--       최소월급이 $6,000 초과여야 한다.
+select manager_id, min(salary)
+from employees
+where manager_id is not null
+group by manager_id
+having min(salary) > 6000
+order by 2 desc;
